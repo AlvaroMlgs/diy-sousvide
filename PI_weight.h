d7a0073475cbc6f7a02e5d7623b9ebab4ca133f4
unknown <100303897@alumnos.uc3m.es>
2018-08-30 21:55:01 +0300

    Stop conisdering adding Derivative action in PI_WEIGHT

--- PI_weight.h
+++ PI_weight.h
@@ -2,10 +2,8 @@
 
 #define PI_weight_kp 6
 #define PI_weight_ki 0.05
-//#define PI_weight_kd 0.5
 #define PI_weight_wp 0.7
 #define PI_weight_wi 1
-//#define PI_weight_wd 1
 
 extern int main_cycle_period;   // Definded in arduino-controller.ino (microseconds)
 
